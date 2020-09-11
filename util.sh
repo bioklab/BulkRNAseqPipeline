89e6fe8758fe27d511093247a74ae8d1ce4aca7a
Ke Liu <bioklab@outlook.com>
2020-09-11 10:49:39 -0400

    V1

--- util.sh
+++ util.sh
@@ -1,4 +1,4 @@
 rename -v 's/\.fq$/\.fastq/' *.fq # rename fq to fastq
 
-prefetch  -a "/home/ubuntu/.aspera/cli/bin/ascp|/home/ubuntu/.aspera/cli/etc/asperaweb_id_dsa.openssh" --max-size 1000000000 --option-file exp.txt # use prefecth to downloadsra files
+prefetch  -a "/home/ubuntu/.aspera/cli/bin/ascp|/home/ubuntu/.aspera/cli/etc/asperaweb_id_dsa.openssh" --max-size 1000000000 --option-file exp.txt # use prefecth to download sra files from SRA. In exp.txt, each row contains a SRR id, like SRRxxxx 
 
